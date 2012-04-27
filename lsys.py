fa5d5cd0c0fffae66d83d3ebf7c669018a0e7ed5
Alex Bogart <billyman3@gmail.com>
2012-04-27 13:42:57 -0700

    JSON working!!

--- lsys.py
+++ lsys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
-import json
+import sys, json
 
 def iterate(curr, rules):
 
@@ -9,8 +9,36 @@ def iterate(curr, rules):
 		if(len(r) != 1):
 			raise KeyError("Keys must be of len 1.")
 
-	return ''.join([rules[x] for x in curr])
+	#Some rules can't be overwritten
+	rules['F'] = 'F'
+	rules['+'] = '+'
+	rules['-'] = '-'
+
+	return ''.join([rules[x.upper()] for x in curr]).upper()
+
+def main():
+	try:
+		with open(sys.argv[1]) as f:
+			inp = json.load(f)
+	except IndexError:
+		print('This program requires an input JSON file.')
+
+	try:
+		rules = inp['rules']
+		start = inp['start']
+		if 'growth' in inp:
+			growth = inp['growth']
+		else:
+			growth = .5
+		iters = inp['iters']
+	except Exception, e:
+		raise e
+
+	result = start
+	for i in range(iters):
+		result = iterate(result, rules)
+
+	print(repr(result))
 
 if __name__ == '__main__':
-	r = {"A": "AB", "B": "A"}
-	print(repr(iterate('ABAABB', r)))
+	main()
